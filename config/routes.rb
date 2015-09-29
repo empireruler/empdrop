9f4b06520b39b36f34a1866e4c829da3f3725468
empireruler <james_conigliaro@dropigee.com>
2015-09-29 14:44:18 -0400

    added devise and more page edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome' => 'pages#home'
   root to: 'pages#home'
   get 'about' => 'pages#about'
+  get 'CDNservices' => 'pages#CDNservices'
+  get 'locations' => 'pages#locations'
+  get 'careers' => 'pages#careers'
+  get 'contact' => 'pages#contact'
+  get 'support' => 'pages#support'
+  get 'login' => 'pages#login'
+
 end
